d1067e170831ef385fe2c8c1e0d9e59426639848
raeleus <raymond.ray3k@gmail.com>
2021-11-06 08:04:20 -0700

    Update imports

--- html/src/main/java/com/ray3k/liftoff/gwt/GwtLauncher.java
+++ html/src/main/java/com/ray3k/liftoff/gwt/GwtLauncher.java
@@ -3,24 +3,28 @@ package com.ray3k.liftoff.gwt;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.graphics.g2d.freetype.gwt.FreetypeInjector;
+import com.badlogic.gdx.graphics.g2d.freetype.gwt.inject.OnCompletion;
 import com.ray3k.liftoff.Core;
 
 /** Launches the GWT application. */
 public class GwtLauncher extends GwtApplication {
-		@Override
-		public GwtApplicationConfiguration getConfig () {
-			// Resizable application, uses available space in browser with no padding:
-			GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
-			cfg.padVertical = 0;
-			cfg.padHorizontal = 0;
-			return cfg;
-			// If you want a fixed size application, comment out the above resizable section,
-			// and uncomment below:
-			//return new GwtApplicationConfiguration(640, 480);
-		}
+	@Override
+	public GwtApplicationConfiguration getConfig() {
 
-		@Override
-		public ApplicationListener createApplicationListener () {
-			return new Core();
-		}
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
+		cfg.padVertical = 0;
+		cfg.padHorizontal = 0;
+		return cfg;
+	}
+	
+	@Override
+	public ApplicationListener createApplicationListener() {
+		return new Core();
+	}
+	
+	@Override
+	public void onModuleLoad() {
+		FreetypeInjector.inject(GwtLauncher.super::onModuleLoad);
+	}
 }
