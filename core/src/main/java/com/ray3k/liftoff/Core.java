9e09716028a407321b195521e863a021d5a98ed5
raeleus <raymond.ray3k@gmail.com>
2021-11-06 06:55:13 -0700

    Initial commit

---
+++ core/src/main/java/com/ray3k/liftoff/Core.java
@@ -0,0 +1,7 @@
+package com.ray3k.liftoff;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Core extends ApplicationAdapter {
+}
