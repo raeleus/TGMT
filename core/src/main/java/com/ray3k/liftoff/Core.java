f80bc440b3585890c56cf0cf34e1543aad5f0efb
raeleus <raymond.ray3k@gmail.com>
2021-11-06 10:55:24 -0700

    Added test of game layout.

--- core/src/main/java/com/ray3k/liftoff/Core.java
+++ core/src/main/java/com/ray3k/liftoff/Core.java
@@ -4,9 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.ray3k.stripe.FreeTypeSkin;
@@ -15,6 +13,7 @@ public class Core extends ApplicationAdapter {
     public static Skin skin;
     public static Stage stage;
     public static ScreenViewport viewport;
+    private static final String long_text = "The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.";
     
     @Override
     public void create() {
@@ -25,10 +24,31 @@ public class Core extends ApplicationAdapter {
         
         var root = new Table();
         root.setFillParent(true);
+        root.setBackground(skin.getDrawable("bg-10"));
         stage.addActor(root);
         
+        var table = new Table();
+        var top = new ScrollPane(table, skin, "panel");
+        top.setFadeScrollBars(false);
+        
+        var label = new Label(long_text, skin);
+        label.setWrap(true);
+        table.add(label).growX();
+        
+        table = new Table();
+        var bottom = new ScrollPane(table, skin, "panel");
+        bottom.setFadeScrollBars(false);
+        
         var textButton = new TextButton("test", skin);
-        root.add(textButton);
+        table.add(textButton).expandX().left();
+        
+        table.row();
+        label = new Label(long_text, skin);
+        label.setWrap(true);
+        table.add(label).growX();
+    
+        var splitPane = new SplitPane(top, bottom, true, skin);
+        root.add(splitPane).grow();
     }
     
     @Override
