c7781989314a4a4b3d618dde6df1864375ed99a0
raeleus <raymond.ray3k@gmail.com>
2021-11-06 08:50:28 -0700

    Added skin and test stage.

--- core/src/main/java/com/ray3k/liftoff/Core.java
+++ core/src/main/java/com/ray3k/liftoff/Core.java
@@ -1,7 +1,53 @@
 package com.ray3k.liftoff;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.ray3k.stripe.FreeTypeSkin;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Core extends ApplicationAdapter {
+    public static Skin skin;
+    public static Stage stage;
+    public static ScreenViewport viewport;
+    
+    @Override
+    public void create() {
+        skin = new FreeTypeSkin(Gdx.files.internal("skin/skin.json"));
+        viewport = new ScreenViewport();
+        stage = new Stage(viewport);
+        Gdx.input.setInputProcessor(stage);
+        
+        var root = new Table();
+        root.setFillParent(true);
+        stage.addActor(root);
+        
+        var textButton = new TextButton("test", skin);
+        root.add(textButton);
+    }
+    
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+    
+    @Override
+    public void render() {
+        ScreenUtils.clear(Color.GREEN);
+        
+        viewport.apply();
+        stage.act();
+        stage.draw();
+    }
+    
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
 }
