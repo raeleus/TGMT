79e5572c5b5081b33d87a2c5c6bd2dd6e62f44de
raeleus <raymond.ray3k@gmail.com>
2021-11-06 07:10:11 -0700

    Added editor project.

---
+++ editor/src/main/java/com/ray3k/liftoff/editor/EditorLauncher.java
@@ -0,0 +1,25 @@
+package com.ray3k.liftoff.editor;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.ray3k.liftoff.Core;
+
+public class EditorLauncher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static Lwjgl3Application createApplication() {
+		return new Lwjgl3Application(new Core(), getDefaultConfiguration());
+	}
+
+	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("tgmt");
+		configuration.useVsync(true);
+		configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+		configuration.setWindowedMode(640, 480);
+		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+		return configuration;
+	}
+}
